acbf7a46b60386c146e0b7886d730da13508e6c6
abin-liu <abinn32@163.com>
2019-03-04 10:25:30 +0800

    1.0.3

--- AutoTower/TowerThread.cs
+++ AutoTower/TowerThread.cs
@@ -44,34 +44,34 @@ namespace AutoTower
 			{
 				WaitForTowerUI(0); // 等待回到初始界面
 				DelayBeforeAction();
-				LeftClick(681 + 285 * Slot, 712); // 选择关卡：左/中/右
+				MouseClick(681 + 285 * Slot, 712); // 选择关卡：左/中/右
 
 				// 检查是否需要花钻石重置挑战时间
 				if (WaitForPixel(921, 608, 90, 132, 0, 1500))
 				{
 					DelayBeforeAction();
-					LeftClick(921, 608); // 点击确定
+					MouseClick(921, 608); // 点击确定
 					continue;
 				}
 
 				// 挑战流程
-				WaitForPixel(920, 752, 255, 142, 121); // 等待难度选择界面
+				WaitForPixel(920, 752, 255, 142, 121, 0); // 等待难度选择界面
 				DelayBeforeAction();
-				LeftClick(904, 487 + 132 * Difficulty); // 选择难度：简单/普通/困难
+				MouseClick(904, 487 + 132 * Difficulty); // 选择难度：简单/普通/困难
 
-				WaitForPixel(76, 736, 206, 115, 57); // 等待队伍选择界面
+				WaitForPixel(76, 736, 206, 115, 57, 0); // 等待队伍选择界面
 				DelayBeforeAction(m_rand.Next(2000, 6000)); // 随机等待2-6秒以防游戏断连
-				LeftClick(1485, 832); // 点击战斗图标按钮
+				MouseClick(1485, 832); // 点击战斗图标按钮
 
-				WaitForPixel(1404, 855, 236, 226, 173); // 等待战斗界面
+				WaitForPixel(1404, 855, 236, 226, 173, 0); // 等待战斗界面
 				DelayBeforeAction(m_rand.Next(1000, 3000)); // 随机等待1-3秒以防游戏断连
-				LeftClick(1404, 855); // 点击立即结束按钮
+				MouseClick(1404, 855); // 点击立即结束按钮
 
 				// 等待战斗失败界面
 				if (WaitForPixel(964, 600, 90, 131, 184, 5000))
 				{
 					DelayBeforeAction();
-					LeftClick(859, 803); // 点击返回按钮
+					MouseClick(859, 803); // 点击返回按钮
 				}
 				// 检查是否胜利
 				else if (WaitForPixel(466, 535, 173, 140, 99, 1000) && WaitForPixel(1104, 563, 165, 139, 98, 1000))
